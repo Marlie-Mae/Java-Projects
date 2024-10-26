346ca48dc62d849992461bb1cbc029f2a450bb36
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-10-26 09:20:37 +0800

    Add files via upload
    For those of you who may not now, a factorial is a product of multiplying from 1 until the number. Now, you must make a program that will accept an integer and then print out its factorial using loops.
    Are you up for this task?
    
    Instructions:
    Input a positive integer.
    Using a for() loop, generate the factorial value of the integer and print out the result.
    Tip #1: Factorials work like this: Factorial of 5 = 1 * 2 * 3 * 4 * 5
    Tip #2: There's a special case in factorials. The factorial of 0 is 1.

---
+++ Java For Loop/factorials.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = 1;
+
+        for(int i = 1; i <= a; i++){
+            b = b * i;           
+        }
+
+        System.out.print(b);
+ }
+}
