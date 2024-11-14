dd5d9dcab8c9515e42f986ef0eb5fa8040ea586b
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-14 20:38:48 +0800

    Update SimplePalindromeChecker.java

--- SimplePalindromeChecker.java
+++ SimplePalindromeChecker.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class SimplePalindromeChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
