f32e426687bb680c41437be47ac2a4b5a3c95e11
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-14 20:34:19 +0800

    Update SimplePalindromeChecker.java

--- SimplePalindromeChecker.java
+++ SimplePalindromeChecker.java
@@ -1,10 +1,27 @@
-import java.io*;
+import java.io.*;
 import java.util.Scanner;
 
 public class SimplePalindromeChecker {
-    publci static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter a word or a phrase")
+        System.out.print("Enter a word or a phrase");
+        String input = sc.nextLine();
+        
+        String cleanInput = input.replaceAll("\\s+","").toLowerCase();
+        
+        String reverse  = "";
+        
+        for (int i = cleanInput.length() - 1; i >= 0; i--) {
+            reversed += cleanInput.charAt(i);
+        }
+        
+        if (cleanInput.equals(reversed)) {
+            System.out.println("The input is a palindrome.");
+        } else {
+            System.out.println("The input is not a palindrome.");
+        }
+        
+        sc.close();
     }
 }
