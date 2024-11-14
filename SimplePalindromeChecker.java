45fa3e93ab77b891fcdffccd3c191a5ca3f93bdd
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-11-14 20:14:41 +0800

    Create SimplePalindromeChecker.java

---
+++ SimplePalindromeChecker.java
@@ -0,0 +1,10 @@
+import java.io*;
+import java.util.Scanner;
+
+public class SimplePalindromeChecker {
+    publci static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a word or a phrase")
+    }
+}
